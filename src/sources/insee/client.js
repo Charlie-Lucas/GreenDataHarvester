11a1a3a0d0631a4517b18d04c6c79cdfddc0c40d
Charlie Lucas <charlielucas.cl@gmail.com>
2025-08-08 19:16:58 +0200

    feat: add configurable connectors and CI workflow

--- src/sources/insee/client.js
+++ src/sources/insee/client.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 
 async function fetchInsee(endpoint, token) {
   const response = await axios.get(endpoint, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
+    proxy: false
   });
   return response.data;
 }
